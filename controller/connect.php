2c39b8b08791c4596d8963101d73315744bddab9
Somchai <somchai.b@cvc.ac.th>
2020-08-10 14:11:26 +0700

    add if $conn

--- controller/connect.php
+++ controller/connect.php
@@ -3,3 +3,6 @@
 $conn = new mysqli('remotemysql.com','RQrfN7VYpv','tI1f03HRTH','RQrfN7VYpv');
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
+if(!$conn){
+    echo "<h1> ไม่สามารถเชื่อมต่อฐานข้อมูลได้ </h1>";
+}

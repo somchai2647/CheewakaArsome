5b2d60aea204adb65ac4fb5dfa8c2f9806d1b6b9
Somchai <somchai.b@cvc.ac.th>
2020-08-15 14:50:48 +0700

    fix noti

--- controller/load_noti.php
+++ controller/load_noti.php
@@ -3,7 +3,7 @@ include "connect.php";
 $user_id = $_SESSION['user_id'];
 // notification_user
 if ($_SESSION['user_level'] == 0 or $_SESSION['user_level'] == 3) {
-    $sql = "SELECT COUNT(*) AS wait_num FROM `order` WHERE `order_status` = 'w'";
+    $sql = "SELECT COUNT(*) AS wait_num FROM `order` WHERE `order_status` = 'w' AND user_id = '$user_id'";
     // $_SESSION['noti_user'][0] = $noti['wait_num'] = $noti = $conn->query($sql)->fetch_array();
     $resultnoti = $conn->query($sql);
     $noti = $resultnoti->fetch_assoc();
